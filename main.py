289f67027e464162418c136b50242c78f60d92bb
Atta <attasenior@gmail.com>
2024-07-05 04:27:51 +0000

    added project and blog html

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template('index.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
